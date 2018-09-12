e284687ddb2a2bd8614e23e917c130c79239fec2
lebogan <lewis.bogan@comcast.net>
2018-09-12 10:27:48 -0700

    prepare snmp redactor

--- spec/snmp_spec.cr
+++ spec/snmp_spec.cr
@@ -2,8 +2,8 @@ require "./spec_helper"
 require "../src/snob/snmp"
 
 describe Snmp do
-  describe "initialize" do
-    it "Creates a new Snmp object." do
+  describe "#initialize" do
+    it "creates a new Snmp object." do
       host = Snmp::Snmp.new("auth", "priv", "test_user", "crypto")
       host.should be_a(Snmp::Snmp)
     end
